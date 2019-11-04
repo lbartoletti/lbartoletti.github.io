f1c9bdee831574857553002b835f5bef90e1ee3d
lbartoletti <lbartoletti@users.noreply.github.com>
2019-11-04 19:51:04 +0100

    Create handle.bat

---
+++ handle.bat
@@ -0,0 +1,5 @@
+wget https://download.sysinternals.com/files/Handle.zip
+7z x Handle.zip
+for /F "tokens=3,6 delims=: " %%I IN ('handle.exe -accepteula tar.bz2') DO handle.exe -c %%J -y -p %%I
+del handle.exe
+del Handle.zip
