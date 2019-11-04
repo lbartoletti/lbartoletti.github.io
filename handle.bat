52b4d34ab3e9357ae3607232eb012d7ca4882a67
lbartoletti <lbartoletti@users.noreply.github.com>
2019-11-04 20:00:21 +0100

    Update handle.bat

--- handle.bat
+++ handle.bat
@@ -1,5 +1,5 @@
 wget https://download.sysinternals.com/files/Handle.zip
-7z x Handle.zip
+7z x Handle.zip -aoa
 for /F "tokens=3,6 delims=: " %%I IN ('handle.exe -accepteula osgeo4w') DO handle.exe -c %%J -y -p %%I
 del handle.exe
 del Handle.zip
